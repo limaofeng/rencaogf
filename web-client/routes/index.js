e52e3503833bfd27b5c67ef21041f3110f4059ee
肖时初 <skycase@163.com>
2015-03-18 22:56:58 +0800

    案例+设计师

--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -7,19 +7,18 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index',{menus:{index:true},partials: {header:'header',footer:'footer'}});
 });
-
 router.get('/collection', function (req, res) {
-    res.render('index',{menus:{collection:true},partials: {header:'header',footer:'footer'}});
+    res.render('collection',{menus:{collection:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
 router.get('/collection_detail', function (req, res) {
-    res.render('index',{menus:{collection_detail:true},partials: {header:'header',footer:'footer'}});
+    res.render('collection_detail',{menus:{collection:true},partials: {header:'header',footer:'footer'}});
 });
 
 router.get('/design', function (req, res) {
-    res.render('index',{menus:{design:true},partials: {header:'header',footer:'footer'}});
+    res.render('design',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
 router.get('/design_detail', function (req, res) {
-    res.render('index',{menus:{design_detail:true},partials: {header:'header',footer:'footer'}});
+    res.render('design_detail',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
 
 module.exports = router;
