fcad6adea2d9fc05222acc5d1099878778350995
李茂峰 <limaofeng@msn.com>
2015-03-19 09:41:44 +0800

    访问路径调整

--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -7,18 +7,20 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index',{menus:{index:true},partials: {header:'header',footer:'footer'}});
 });
-router.get('/collection', function (req, res) {
-    res.render('collection',{menus:{collection:true},partials: {header:'header',page:'page',footer:'footer'}});
+router.get('/cases', function (req, res) {
+    res.render('cases/index',{menus:{collection:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
-router.get('/collection_detail', function (req, res) {
-    res.render('collection_detail',{menus:{collection:true},partials: {header:'header',footer:'footer'}});
+router.get('/cases/:id', function (req, res) {
+    console.log(req.params.id);
+    res.render('cases/details',{menus:{collection:true},partials: {header:'header',footer:'footer'}});
 });
 
-router.get('/design', function (req, res) {
-    res.render('design',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
+router.get('/designers', function (req, res) {
+    res.render('designers/index',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
-router.get('/design_detail', function (req, res) {
-    res.render('design_detail',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
+router.get('/designers/:id', function (req, res) {
+    console.log(req.params.id);
+    res.render('designers/details',{menus:{design:true},partials: {header:'header',page:'page',footer:'footer'}});
 });
 
 module.exports = router;
